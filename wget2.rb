2987a789c7645bde7d5f973c6f8d87c53b03575c
Rob Bajorek <Robert.Bajorek@target.com>
2019-09-06 15:03:43 -0500

    Added wget2 formula.

---
+++ wget2.rb
@@ -0,0 +1,24 @@
+class Wget2 < Formula
+  desc "File and recursive website downloader. Successor to wget."
+  homepage "https://gitlab.com/gnuwget/wget2/"
+  url "https://ftp.gnu.org/gnu/wget/wget2-1.99.2.tar.gz"
+  sha256 "cbc48f55fa22ed2acbccf032c208c133cc59c7432cda8518a4992eb5882b6563"
+
+  depends_on "gettext"
+  depends_on "gnutls"
+  depends_on "gpgme"
+  depends_on "libpsl"
+  depends_on "nghttp2"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"wget2", "-O", "/dev/null", "https://www.gnu.org/"
+  end
+end
